669d0a36f7720f1edde115c02c27a4ea63af5c2f
Dev Atomic <devatomic@Devs-MacBook-Pro.local>
2023-09-04 11:08:24 -0300

    Feat: PWA config

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @next/next/no-title-in-document-head */
 
-import { Head, Html, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
@@ -14,81 +14,70 @@ export default function Document() {
 
         <meta content="Perfarm App" name="application-name" />
         <meta content="yes" name="apple-mobile-web-app-capable" />
-        <meta content="default" name="apple-mobile-web-app-status-bar-style" />
+        <meta content="black-translucent" name="apple-mobile-web-app-status-bar-style" />
         <meta content="Perfarm App" name="apple-mobile-web-app-title" />
-        <meta
-          content="Gestão financeira e estratégica de fazendas."
-          name="description"
-        />
+        <meta content="Gestão financeira e estratégica de fazendas." name="description" />
         <meta content="telephone=no" name="format-detection" />
         <meta content="yes" name="mobile-web-app-capable" />
         <meta content="#FDCC06" name="msapplication-TileColor" />
         <meta content="no" name="msapplication-tap-highlight" />
-        <meta content="#43b874" name="theme-color" />
+        <meta content="#43B874" name="theme-color" />
 
         <link href="/manifest.json" rel="manifest" />
 
         <meta content="website" property="og:type" />
         <meta content="Perfarm" property="og:title" />
-        <meta
-          content="Gestão financeira e estratégica de fazendas."
-          property="og:description"
-        />
+        <meta content="Gestão financeira e estratégica de fazendas." property="og:description" />
         <meta content="Perfarm" property="og:site_name" />
         <meta content="https://www.perfarm.com/" property="og:url" />
-        <meta
-          content="https://www.perfarm.com/static/favicon/favicon-256.png"
-          property="og:image"
-        />
+        <meta content="https://www.perfarm.com/static/favicon/favicon-256.png" property="og:image" />
+
+        <link color="#43b874" href="/static/favicon/favicon-mask.svg" rel="mask-icon" />
+        <link href="/static/favicon/favicon.svg" rel="icon" />
+        <link href="/static/favicon/favicon-16.png" rel="icon" type="image/png" />
+        <link href="/static/favicon/favicon-32.png" rel="icon" sizes="32x32" type="image/png" />
+        <link href="/static/favicon/favicon-48.png" rel="icon" sizes="48x48" type="image/png" />
+        <link href="/static/favicon/favicon-62.png" rel="icon" sizes="62x62" type="image/png" />
+        <link href="/static/favicon/favicon-192.png" rel="icon" sizes="192x192" type="image/png" />
+        <link href="/static/favicon/favicon-200.png" rel="icon" sizes="200x200" type="image/png" />
+        <link href="/static/favicon/favicon-256.png" rel="icon" sizes="256x256" type="image/png" />
+        <link href="/static/favicon/favicon-192.png" rel="apple-touch-icon" />
 
         <link
-          color="#43b874"
-          href="/static/favicon/favicon-mask.svg"
-          rel="mask-icon"
-        />
-        <link href="/static/favicon/favicon.svg" rel="icon" />
-        <link
-          href="/static/favicon/favicon-16.png"
-          rel="icon"
-          type="image/png"
+          rel="apple-touch-startup-image"
+          href="images/splash/launch-640x1136.png"
+          media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
         />
         <link
-          href="/static/favicon/favicon-32.png"
-          rel="icon"
-          sizes="32x32"
-          type="image/png"
+          rel="apple-touch-startup-image"
+          href="images/splash/launch-750x1294.png"
+          media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
         />
         <link
-          href="/static/favicon/favicon-48.png"
-          rel="icon"
-          sizes="48x48"
-          type="image/png"
+          rel="apple-touch-startup-image"
+          href="images/splash/launch-1242x2148.png"
+          media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
         />
         <link
-          href="/static/favicon/favicon-62.png"
-          rel="icon"
-          sizes="62x62"
-          type="image/png"
+          rel="apple-touch-startup-image"
+          href="images/splash/launch-1125x2436.png"
+          media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
         />
         <link
-          href="/static/favicon/favicon-192.png"
-          rel="icon"
-          sizes="192x192"
-          type="image/png"
+          rel="apple-touch-startup-image"
+          href="images/splash/launch-1536x2048.png"
+          media="(min-device-width: 768px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"
         />
         <link
-          href="/static/favicon/favicon-200.png"
-          rel="icon"
-          sizes="200x200"
-          type="image/png"
+          rel="apple-touch-startup-image"
+          href="images/splash/launch-1668x2224.png"
+          media="(min-device-width: 834px) and (max-device-width: 834px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"
         />
         <link
-          href="/static/favicon/favicon-256.png"
-          rel="icon"
-          sizes="256x256"
-          type="image/png"
+          rel="apple-touch-startup-image"
+          href="images/splash/launch-2048x2732.png"
+          media="(min-device-width: 1024px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"
         />
-        <link href="/static/favicon/favicon-192.png" rel="apple-touch-icon" />
 
         <title>Perfarm Experience</title>
       </Head>
--- src/screens/News/index.tsx
+++ src/screens/News/index.tsx
@@ -2,35 +2,40 @@ import { FC } from 'react';
 
 import NewsMock from '~/commons/mock/news';
 
+import { useRouter } from 'next/router';
 import { Card } from '~/components/Card';
 import { CaretLeft } from '~/components/Icons/CaretLeft';
 import { TwoColorsContainer } from '~/components/TwoColorsContainer';
 import { Typography } from '~/components/Typography';
 import { Description, ImageContainer, ImageContent, Page, Title, TitleContainer } from './style';
 
-export const News: FC = () => (
-  <Page>
-    <TwoColorsContainer>
-      <TitleContainer>
-        <CaretLeft size={24} color="white" />
-        <Typography color="$white" weight="$bold" variant="$body2">
-          Notícia
-        </Typography>
-      </TitleContainer>
+export const News: FC = () => {
+  const { back } = useRouter();
 
-      <Card style={{ maxWidth: 500, padding: 15, width: '100%' }}>
-        <ImageContainer>
-          <ImageContent style={{ backgroundImage: `url(${NewsMock.image})` }}></ImageContent>
-        </ImageContainer>
+  return (
+    <Page>
+      <TwoColorsContainer>
+        <TitleContainer onClick={back}>
+          <CaretLeft size={24} color="white" />
+          <Typography color="$white" weight="$bold" variant="$body2">
+            Notícia
+          </Typography>
+        </TitleContainer>
 
-        <Title color="$pastureGreen" variant="$body2" weight="$bold">
-          {NewsMock.title}
-        </Title>
+        <Card style={{ maxWidth: 500, padding: 15, width: '100%' }}>
+          <ImageContainer>
+            <ImageContent style={{ backgroundImage: `url(${NewsMock.image})` }}></ImageContent>
+          </ImageContainer>
 
-        <Description color="$gray" variant="$body6">
-          {NewsMock.content.trim()}
-        </Description>
-      </Card>
-    </TwoColorsContainer>
-  </Page>
-);
+          <Title color="$pastureGreen" variant="$body2" weight="$bold">
+            {NewsMock.title}
+          </Title>
+
+          <Description color="$gray" variant="$body6">
+            {NewsMock.content.trim()}
+          </Description>
+        </Card>
+      </TwoColorsContainer>
+    </Page>
+  );
+};
