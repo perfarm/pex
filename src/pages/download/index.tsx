84d36d278c43ac8ed2869bfe672af12cf6c840a4
Joseafs <jose.fernandes@seedz.ag>
2023-09-06 16:11:01 -0300

    fix: remove footer of /download

--- src/pages/download/index.tsx
+++ src/pages/download/index.tsx
@@ -1,9 +1,6 @@
-import { TabLayout } from '~/components/TabLayout';
 import { ScreenDownload } from '~/screens/download';
 import { NextPageWithLayout } from '../_app';
 
 const Download: NextPageWithLayout = () => <ScreenDownload />;
-
-Download.getLayout = (page) => <TabLayout>{page}</TabLayout>;
 
 export default Download;
