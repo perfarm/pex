31c3528414551cd80854d62c055b1c7fc8839e54
Joseafs <jose.fernandes@seedz.ag>
2023-09-06 10:09:50 -0300

    wip: serviceWorker in navigator

--- src/pages/download/index.tsx
+++ src/pages/download/index.tsx
@@ -1,9 +1,25 @@
+import useDidMount from 'beautiful-react-hooks/useDidMount';
 import { useEffect, useState } from 'react';
 import { ScreenDownload } from '~/screens/download';
 import { NextPageWithLayout } from '../_app';
 
 const Download: NextPageWithLayout = () => {
   const [deferredPrompt, setDeferredPrompt] = useState(null);
+
+  useDidMount(() => {
+    console.log('tte--useDidMount - serviceWorker');
+
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/sw.js')
+        .then(function (registration) {
+          console.log('tte--Service Worker registered with scope:', registration.scope);
+        })
+        .catch(function (error) {
+          console.error('tte--Service Worker registration failed:', error);
+        });
+    }
+  });
 
   useEffect(() => {
     const deferredReady = (e: any) => {
@@ -12,13 +28,13 @@ const Download: NextPageWithLayout = () => {
 
     if (typeof window !== 'undefined') {
       console.log('tte--addEventListener-beforeinstallprompt1');
-      window.addEventListener('beforeinstallprompt', deferredReady);
+      window.addEventListener('BeforeInstallPromptEvent', deferredReady);
       console.log('tte--addEventListener-beforeinstallprompt2');
     }
 
     return () => {
       console.log('tte--removeEventListener-beforeinstallprompt1');
-      window.removeEventListener('beforeinstallprompt', deferredReady);
+      window.removeEventListener('BeforeInstallPromptEvent', deferredReady);
       console.log('tte--removeEventListener-beforeinstallprompt2');
     };
   }, []);
@@ -31,6 +47,7 @@ const Download: NextPageWithLayout = () => {
       deferredPrompt.prompt();
       const { outcome } = await deferredPrompt.userChoice;
       if (outcome === 'accepted') {
+        console.log('tte--click deferredPrompt null');
         setDeferredPrompt(null);
       }
     }
